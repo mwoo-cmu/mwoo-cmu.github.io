3886929bf0a591e8e85f5b21446c52ec29c1eacb
mwoo-cmu <mwoo2@alumni.cmu.edu>
2024-11-19 16:47:19 -0800

    try init?

--- tools/open_canto/index.js
+++ tools/open_canto/index.js
@@ -1,7 +1,11 @@
-import {CongkitFilter, CongkitVersion, Dict} from "./pkg/open_canto.js"
+import init, {CongkitFilter, CongkitVersion, Dict} from "./pkg/open_canto.js"
 
-var filters = new CongkitFilter();
-filters.chinese = true;
-filters.big5 = true;
-filters.hkscs = true;
-var dict = Dict.new(CongkitVersion.V3, filters);
+function run() {
+  var filters = new CongkitFilter();
+  filters.chinese = true;
+  filters.big5 = true;
+  filters.hkscs = true;
+  var dict = Dict.new(CongkitVersion.V3, filters); 
+}
+
+init().then(run)

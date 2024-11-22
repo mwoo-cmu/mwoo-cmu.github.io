abfbfa85e42f3d13cd81671500190efc206e9a5f
mwoo-cmu <mwoo2@alumni.cmu.edu>
2024-11-21 23:17:09 -0800

    remove all calls to bitcode for testing

--- tools/open_canto/pkg/open_canto.js
+++ tools/open_canto/pkg/open_canto.js
@@ -940,7 +940,7 @@ function __wbg_get_imports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper4520 = function() { return logError(function (arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper4339 = function() { return logError(function (arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 299, __wbg_adapter_32);
         return addHeapObject(ret);
     }, arguments) };
